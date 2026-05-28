c401267e29e9f1217884c42ac57f310badc365c1
dhinesh-mern <rcdanteff@gmail.com>
2026-05-28 17:19:23 +0530

    img added

--- dhinesh_estate_123/__manifest__.py
+++ dhinesh_estate_123/__manifest__.py
@@ -1,10 +1,29 @@
 {
-    'name': 'dhinesh_estate_123',
+    'name': 'Dhinesh Estate 123',
     'version': '1.0',
+    'summary': 'Estate Management Module',
+    'description': """
+Estate Management Module for Odoo 17
+===================================
+
+Features:
+- Property Management
+- Property Types
+- Menu Management
+- Security Groups
+""",
+
+    'author': 'Dhinesh Kumar',
+    'website': 'https://github.com/dhinesh-mern',
+    'category': 'Real Estate',
+    'license': 'LGPL-3',
+
     'depends': ['base'],
+
     'application': True,
+
     'data': [
-        
+
         # security
         'security/res_groups.xml',
         'security/ir.model.access.csv',
@@ -14,7 +33,12 @@
         'views/property_type_views.xml',
         'views/estate_menu.xml',
     ],
-    'demo':[
-        "demo/demo.xml"
-    ]
+
+    'demo': [
+        'demo/demo.xml'
+    ],
+
+    'images': [
+        'static/description/banner.png',
+    ],
 }
